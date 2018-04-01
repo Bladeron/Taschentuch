d03c4113907ceb426b0e0595d91c293b3c411b41
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-01 19:01:06 +0200

    Se mueven los dos players pero no simultaneamente

--- js/player.js
+++ js/player.js
@@ -5,6 +5,7 @@ function Player(game,x, y, color) {
   this.x = x;
   this.y = y;
   this.color = color;
+  this.score = 0;
   
   this.vx = 10;
   this.vy = 10;
@@ -19,17 +20,29 @@ Player.prototype.setListeners = function() {
   document.onkeydown = function(event) {
     switch( event.keyCode) {
       case 38: // Up
-        this.y -= this.vy;
+        this.game.player.y -= this.vy;
         break;
       case 40: // Down
-         this.y += this.vy;
+        this.game.player.y += this.vy;
          break;
       case 37: // left
-        this.x -= this.vx;
+        this.game.player.x -= this.vx;
         break;
       case 39: // right
-        this.x += this.vx;
+        this.game.player.x += this.vx;
         break;
+      case 87:  //Up
+        this.game.player2.y -= this.vy;
+        break;
+      case 83:  //Down
+        this.game.player2.y += this.vy;
+        break;
+      case 65:  //Left
+        this.game.player2.x -= this.vy;
+        break;
+      case 68:  //Right
+        this.game.player2.x += this.vy;
+        break;  
       }
   }.bind(this);
 };
