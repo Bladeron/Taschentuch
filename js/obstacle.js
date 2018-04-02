bf643bc097e61e25b53979a85980aeaed4952b0a
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-02 13:23:43 +0200

    Obstaculo de prueba a mano

--- js/obstacle.js
+++ js/obstacle.js
@@ -1,7 +1,16 @@
-function Obstacle(x, y) {
+function Obstacle(game) {
   this.game = game;
+
   
-  this.x = x;
-  this.y = y;
+  this.x = 460;
+  this.y = 300;
+
+  this.w = 30;
+  this.h = 300;
 
 }
+
+Obstacle.prototype.draw = function() {
+  this.game.ctx.fillStyle = "blue";
+  this.game.ctx.fillRect(this.x, this.y, this.w, this.h);
+};
